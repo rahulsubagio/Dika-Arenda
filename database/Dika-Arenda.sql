0df376336584b4ca4b57f7fdc7668c747ce2595c
rahulsubagio <rahulsubagio99@gmail.com>
2021-03-05 10:45:58 +0700

    aplod database

---
+++ database/Dika-Arenda.sql
@@ -0,0 +1,443 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 05 Mar 2021 pada 04.44
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Dika-Arenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customer`
+--
+
+CREATE TABLE `customer` (
+  `code` varchar(10) NOT NULL,
+  `nama_customer` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penyusutan`
+--
+
+CREATE TABLE `detail_penyusutan` (
+  `id_produk` int(11) NOT NULL,
+  `id_penyusutan` int(11) NOT NULL,
+  `mati_kandang_ekor` int(11) NOT NULL,
+  `mati_kandang_kg` float NOT NULL,
+  `mati_armada_ekor` int(11) NOT NULL,
+  `mati_armada_kg` float NOT NULL,
+  `mati_rpa_ekor` int(11) NOT NULL,
+  `mati_rpa_kg` float NOT NULL,
+  `admin_ekor` int(11) NOT NULL,
+  `admin_kg` float NOT NULL,
+  `riil_ekor` int(11) NOT NULL,
+  `riil_kg` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `neraca`
+--
+
+CREATE TABLE `neraca` (
+  `id_neraca` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `laba_rugi` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `neraca_penjualan`
+--
+
+CREATE TABLE `neraca_penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_neraca` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `neraca_transaksi`
+--
+
+CREATE TABLE `neraca_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_neraca` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id_pegawai` int(11) NOT NULL,
+  `nama_pegawai` varchar(255) NOT NULL,
+  `telp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `ekor` int(11) NOT NULL,
+  `kg` float NOT NULL,
+  `harga` int(11) NOT NULL,
+  `a_kompensasi` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `pembayaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penyusutan`
+--
+
+CREATE TABLE `penyusutan` (
+  `id_penyusutan` int(11) NOT NULL,
+  `jumlah_ekor` int(11) NOT NULL,
+  `jumlah_kg` int(11) NOT NULL,
+  `persentase` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `stok_ekor` int(100) NOT NULL,
+  `stok_kg` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rekening`
+--
+
+CREATE TABLE `rekening` (
+  `id_rekening` int(11) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `saldo_akhir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_vendor` int(11) NOT NULL,
+  `id_pegawai` int(11) NOT NULL,
+  `no_surat` varchar(255) NOT NULL,
+  `jenis_transaksi` varchar(255) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `ekor` int(11) NOT NULL,
+  `kg` float NOT NULL,
+  `harga` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `pembayaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `vendor`
+--
+
+CREATE TABLE `vendor` (
+  `id_vendor` int(11) NOT NULL,
+  `nama_vendor` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `telp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- Indeks untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD KEY `fk_detail_penjualan` (`id_penjualan`),
+  ADD KEY `fk_detail_penjualan_produk` (`id_produk`);
+
+--
+-- Indeks untuk tabel `detail_penyusutan`
+--
+ALTER TABLE `detail_penyusutan`
+  ADD KEY `fk_penyusutan_produk` (`id_produk`),
+  ADD KEY `fk_penyusutan` (`id_penyusutan`);
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `fk_detail_transaksi` (`id_transaksi`),
+  ADD KEY `fk_detail_transaksi_produk` (`id_produk`);
+
+--
+-- Indeks untuk tabel `neraca`
+--
+ALTER TABLE `neraca`
+  ADD PRIMARY KEY (`id_neraca`);
+
+--
+-- Indeks untuk tabel `neraca_penjualan`
+--
+ALTER TABLE `neraca_penjualan`
+  ADD KEY `fk_neraca_penjualan` (`id_penjualan`),
+  ADD KEY `fk_neraca_p` (`id_neraca`);
+
+--
+-- Indeks untuk tabel `neraca_transaksi`
+--
+ALTER TABLE `neraca_transaksi`
+  ADD KEY `fk_transaksi_neraca` (`id_transaksi`),
+  ADD KEY `fk_neraca` (`id_neraca`);
+
+--
+-- Indeks untuk tabel `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id_pegawai`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `fk_code_penjualan` (`code`);
+
+--
+-- Indeks untuk tabel `penyusutan`
+--
+ALTER TABLE `penyusutan`
+  ADD PRIMARY KEY (`id_penyusutan`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indeks untuk tabel `rekening`
+--
+ALTER TABLE `rekening`
+  ADD PRIMARY KEY (`id_rekening`),
+  ADD KEY `fk_code` (`code`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_vendor` (`id_vendor`),
+  ADD KEY `fk_pegawai` (`id_pegawai`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indeks untuk tabel `vendor`
+--
+ALTER TABLE `vendor`
+  ADD PRIMARY KEY (`id_vendor`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `neraca`
+--
+ALTER TABLE `neraca`
+  MODIFY `id_neraca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penyusutan`
+--
+ALTER TABLE `penyusutan`
+  MODIFY `id_penyusutan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `rekening`
+--
+ALTER TABLE `rekening`
+  MODIFY `id_rekening` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `vendor`
+--
+ALTER TABLE `vendor`
+  MODIFY `id_vendor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD CONSTRAINT `fk_detail_penjualan` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id_penjualan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_detail_penjualan_produk` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `detail_penyusutan`
+--
+ALTER TABLE `detail_penyusutan`
+  ADD CONSTRAINT `fk_penyusutan` FOREIGN KEY (`id_penyusutan`) REFERENCES `penyusutan` (`id_penyusutan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_penyusutan_produk` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `fk_detail_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_detail_transaksi_produk` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `neraca_penjualan`
+--
+ALTER TABLE `neraca_penjualan`
+  ADD CONSTRAINT `fk_neraca_p` FOREIGN KEY (`id_neraca`) REFERENCES `neraca` (`id_neraca`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_neraca_penjualan` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id_penjualan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `neraca_transaksi`
+--
+ALTER TABLE `neraca_transaksi`
+  ADD CONSTRAINT `fk_neraca` FOREIGN KEY (`id_neraca`) REFERENCES `neraca` (`id_neraca`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_transaksi_neraca` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `fk_code_penjualan` FOREIGN KEY (`code`) REFERENCES `customer` (`code`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `rekening`
+--
+ALTER TABLE `rekening`
+  ADD CONSTRAINT `fk_code` FOREIGN KEY (`code`) REFERENCES `customer` (`code`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_pegawai` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_vendor` FOREIGN KEY (`id_vendor`) REFERENCES `vendor` (`id_vendor`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
