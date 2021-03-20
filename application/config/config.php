be0d557b12612dac5fd102dcf5e51f10008a1a65
rahulsubagio <rahulsubagio99@gmail.com>
2021-03-20 11:18:22 +0700

    ganti base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.100.80/Dika-Arenda/';
+$config['base_url'] = 'http://localhost/Dika-Arenda/';
 
 /*
 |--------------------------------------------------------------------------
