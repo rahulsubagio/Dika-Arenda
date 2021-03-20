493666a07b512d82ef35f578da5b228014977287
rahulsubagio <rahulsubagio99@gmail.com>
2021-03-20 11:16:05 +0700

    buat template nav & sidebar

---
+++ application/views/templates/footer.php
@@ -0,0 +1,12 @@
+</div>
+</div>
+
+
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
+<script src="<?= base_url('assets/js/dashboard.js') ?>"></script>
+</body>
+
+</html>
