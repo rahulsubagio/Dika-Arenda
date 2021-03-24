1e20a89987ea77cf5b1cbb5f2088c899ca6c4ff3
rahulsubagio <rahulsubagio99@gmail.com>
2021-03-25 00:29:37 +0700

    tambah contact view

---
+++ application/views/home/contact.php
@@ -0,0 +1,13 @@
+<main>
+  <div class="card shadow p-3">
+    <div class="card-body row">
+      <h1 class="mb-4 fw-bold" style="color: #4e73df;">Contact</h1>
+      <div class="col-6">
+        <img src="<?= base_url('assets/gambar/contact.png') ?>" width="250">
+      </div>
+      <div class="col-6">
+        <p style="text-align: justify; color: #5a5c69;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias repudiandae eum nihil pariatur eaque ut? Voluptates, aliquam autem expedita, adipisci ratione veniam illo molestias earum nam quisquam vero omnis tenetur!</p>
+      </div>
+    </div>
+  </div>
+</main>
