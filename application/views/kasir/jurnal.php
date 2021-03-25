13194518368accf79d1909122adcd97fb899323a
rifkacanalisa <50352632+rifkacanalisa@users.noreply.github.com>
2021-03-25 07:35:34 +0700

    tambah halamn jurnal dan edit untuk active dll

---
+++ application/views/kasir/jurnal.php
@@ -0,0 +1,79 @@
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2 text-secondary">Jurnal Transaksi</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+      <div class="btn-group me-2">
+        <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+        <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+      </div>
+      <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+        <span data-feather="calendar"></span>
+        This week
+      </button>
+    </div>
+  </div>
+  <table class="table table-hover">
+    <thead class="table-light">
+      <tr align="center">
+        <th>No.</th>
+        <th>Customer</th>
+        <th>Code</th>
+        <th>Ekor</th>
+        <th>Kg</th>
+        <th>Harga</th>
+        <th>Jumlah</th>
+        <th>a</th>
+        <th>Total</th>
+        <th>Pembayaran</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td align="center">1.</td>
+        <td>Rahul Subagio</td>
+        <td align="center">C12</td>
+        <td align="center">4</td>
+        <td align="center">5,1</td>
+        <td align="center">21.000</td>
+        <td align="center">107.100</td>
+        <td align="center">-</td>
+        <td align="center">107.100</td>
+        <td align="center">107.100</td>
+        <td align="center">
+          <button class="btn btn-primary">Ubah</button>
+          <button class="btn btn-danger">Hapus</button>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">2.</td>
+        <td>Budi Setiawan</td>
+        <td align="center">C1</td>
+        <td align="center">5</td>
+        <td align="center">5,1</td>
+        <td align="center">21.000</td>
+        <td align="center">107.100</td>
+        <td align="center">-</td>
+        <td align="center">107.100</td>
+        <td align="center">107.100</td>
+        <td align="center">
+          <button class="btn btn-primary">Ubah</button>
+          <button class="btn btn-danger">Hapus</button>
+        </td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="3">Subtotal</td>
+        <td align="center">116</td>
+        <td align="center">122,6</td>
+        <td align="center">22.609</td>
+        <td align="center">2.777.570</td>
+        <td align="center">333</td>
+        <td align="center">2.767.570</td>
+        <td align="center">3.034.920</td>
+        <td align="center"><button class="btn btn-success">Tambah</button></td>
+      </tr>
+    </tfoot>
+  </table>
+</main>
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -32,7 +32,7 @@
 <body>
 
   <header class="navbar navbar-light sticky-top flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fw-bold text-center fs-5 text-white" href="#">Dika Arenda</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fw-bold text-center fs-5 text-white" href="<?= base_url(); ?>">Dika Arenda</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
